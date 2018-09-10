e95753d197865d50a7272dd0b02694b9f9142cb7
martindye <martindye@btinternet.com>
2018-09-10 13:15:11 +0100

    Added support for bonus numbers and config for EuroMillions.

--- Program.cs
+++ Program.cs
@@ -15,24 +15,36 @@ namespace QuantumLottery
         static void Main(string[] args)
         {
             // Test
+            // National lottery: default
             PrintLottoNums();
+
+            // EuroMillions
+            //PrintLottoNums(minValue: 1, maxValue: 50,
+            //               itemsPerLine: 5, numLines: 2,
+            //               bonusNumbers: 2, bonusMinValue: 1, bonusMaxValue: 12, 
+            //               debug: false);
         }
         
-        static void PrintLottoNums()
+        static void PrintLottoNums(int minValue = 1, 
+                                   int maxValue = 59, 
+                                   int itemsPerLine = 6,
+                                   int numLines = 4,
+                                   int bonusNumbers = 0,
+                                   int bonusMinValue = 1,
+                                   int bonusMaxValue = 12,
+                                   bool debug = false,
+                                   string randomHexUrl = "https://qrng.anu.edu.au/ran_hex.php")
         {
-        
-            // Options
-            string randomHexUrl = "https://qrng.anu.edu.au/ran_hex.php";
-            bool debug = false;
-
-            int minValue = 1;
-            int maxValue = 59;
-            int itemsPerLine = 6;
-            int numLines = 4;
-
+            int itemsPerLineWithBonus = itemsPerLine + bonusNumbers;
             byte[] lotteryNums = new byte[itemsPerLine];
             int lineItemCount = 1;
             int lineNumber = 1;
+
+            int currentMinValue = 0;
+            int currentMaxVlaue = 0;
+
+            // Set to true when generating bonus balls
+            bool bonusMode = false;
 
             // Confirmed: server sends out 2-digit hex numbers at a time
 
@@ -53,7 +65,12 @@ namespace QuantumLottery
                 
                 myChar = Convert.ToByte(hexOutput, 16);
 
-                if (myChar >= minValue && myChar <= maxValue) {
+                if (lineItemCount == 1) {
+                    currentMinValue = minValue;
+                    currentMaxVlaue = maxValue;
+                };
+
+                if (myChar >= currentMinValue && myChar <= currentMaxVlaue) {
 
                     if (!Array.Exists(lotteryNums, (byte lot) => { return lot == myChar; } )) {
 
@@ -62,14 +79,43 @@ namespace QuantumLottery
                             Console.WriteLine("Line Number: {0}", lineNumber);
                         };
 
+                        if (bonusMode && lineItemCount == (itemsPerLine + 1)) {
+                            Console.WriteLine();
+                            Console.WriteLine("Bonus numbers:");
+                            Console.WriteLine();
+                        };
+
                         Console.WriteLine(myChar.ToString());
-                        lotteryNums[lineItemCount - 1] = myChar;
+
+                        if (bonusMode) {
+                            // In bonus mode, we increment up to items per line + bonus items
+                            lotteryNums[lineItemCount - itemsPerLine - 1] = myChar;
+                        }
+                        else {
+                            lotteryNums[lineItemCount - 1] = myChar;
+                        };
+
                         lineItemCount++;
 
-                        if (lineItemCount > itemsPerLine) {
+                        if (lineItemCount == (itemsPerLine + 1) &&
+                            lineItemCount <= (itemsPerLine + bonusNumbers) ) {
+                            // Initialise for first bonus item
+                            lotteryNums = new byte[bonusNumbers];
+                            bonusMode = true;
+                            currentMinValue = bonusMinValue;
+                            currentMaxVlaue = bonusMaxValue;
+                        }
+                        else if (lineItemCount >= (itemsPerLine + 2) && 
+                                 lineItemCount <= (itemsPerLine + bonusNumbers) ) {
+                            // All the rest of the nbonus numebrs
+
+                        }
+                        else if (lineItemCount > itemsPerLineWithBonus)
+                        {
+                            lotteryNums = new byte[itemsPerLine];
                             lineItemCount = 1;
                             lineNumber++;
-                            lotteryNums = new byte[itemsPerLine];
+                            bonusMode = false;
                         };
 
                     }
